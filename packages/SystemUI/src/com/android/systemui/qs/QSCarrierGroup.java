c677df13e13b1d8cdc24e90bdc5d3d42c3dec01f
Weijie Wang <weijiew@codeaurora.org>
2018-06-29 17:54:25 +0800

    SystemUI: Add new configuration for displaying Volte icon
    Add new configuration for displaying Volte icon
    Delete some unused code
    
    Change-Id: Idd583ed807b2daad794598c17496bbc99b81a00d
    CRs-Fixed: 2284782

--- packages/SystemUI/src/com/android/systemui/qs/QSCarrierGroup.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSCarrierGroup.java
@@ -206,10 +206,11 @@ public class QSCarrierGroup extends LinearLayout implements
     @Override
     public void setMobileDataIndicators(NetworkController.IconState statusIcon,
             NetworkController.IconState qsIcon, int statusType,
-            int qsType, boolean activityIn, boolean activityOut,
+            int qsType, boolean activityIn, boolean activityOut, int stackedVoiceId,
             CharSequence typeContentDescription,
             CharSequence typeContentDescriptionHtml, CharSequence description,
             boolean isWide, int subId, boolean roaming) {
+
         int slotIndex = getSlotIndex(subId);
         if (slotIndex >= SIM_SLOTS) {
             Log.w(TAG, "setMobileDataIndicators - slot: " + slotIndex);
