e909fa4405df2e14b91833ec00e204b099c6e75b
maxwen <max.weninger@gmail.com>
2018-09-06 13:15:50 +0200

    base: SystemUI: fix brightness slider animation on qs expand
    when on bottom
    
    Change-Id: I94c761dda80a29ab17a7b08ffd1204a20a8fe0d7
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
+++ packages/SystemUI/src/com/android/systemui/qs/QSPanel.java
@@ -113,6 +113,8 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
     private BrightnessMirrorController mBrightnessMirrorController;
     private View mDivider;
 
+    private boolean mBrightnessBottom;
+
     public QSPanel(Context context) {
         this(context, null);
     }
@@ -235,9 +237,11 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
             if (newValue == null || Integer.parseInt(newValue) == 0) {
                 removeView(mBrightnessView);
                 addView(mBrightnessView, 0);
+                mBrightnessBottom = false;
             } else {
                 removeView(mBrightnessView);
                 addView(mBrightnessView, getBrightnessViewPositionBottom());
+                mBrightnessBottom = true;
             }
         }
     }
@@ -808,5 +812,9 @@ public class QSPanel extends LinearLayout implements Tunable, Callback, Brightne
                     setAnimationTile(v);
             });
         }
+    }
+
+    public boolean isBrightnessViewBottom() {
+        return mBrightnessBottom;
     }
 }
