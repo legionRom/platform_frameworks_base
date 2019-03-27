66aadd2ae717376c90693d35ed684ef59f684566
calebcabob <calphonic@gmail.com>
2019-03-26 20:24:30 -0400

    NetworkTraffic: Allow user to choose positioning [1/2]
    This allows the user to choose whether or not they want to see the traffic indicator in status bar or in the expanded header
    
    Change-Id: Ifff10bcccde91ea90f7bfe85acfe70ffc54a9418

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5532,9 +5532,9 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_STATE = "network_traffic_state";
 
         /**
-	 ** What to show in network traffic indicator in statusbar
-	 ** @hide
-	 **/
+         ** What to show in network traffic indicator in statusbar
+         ** @hide
+         **/
         public static final String NETWORK_TRAFFIC_TYPE = "network_traffic_type";
 
         /**
@@ -5550,10 +5550,19 @@ public final class Settings {
         public static final String NETWORK_TRAFFIC_HIDEARROW = "network_traffic_hidearrow";
 
         /**
- 	 ** What size to show for network traffic indicator in statusbar
-	 ** @hide
-	 **/
+         ** What size to show for network traffic indicator in statusbar
+         ** @hide
+         **/
          public static final String NETWORK_TRAFFIC_FONT_SIZE = "network_traffic_font_size";
+
+        /**
+         * Show network traffic indicator
+         * 0 - Statusbar (default)
+         * 1 - QS Header
+         * @hide
+         */
+        public static final String NETWORK_TRAFFIC_VIEW_LOCATION = "network_traffic_view_location";
+
 
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
@@ -5795,6 +5804,12 @@ public final class Settings {
             PRIVATE_SETTINGS.add(CUSTOM_DEVICE_PROXI_CHECK_ENABLED);
             PRIVATE_SETTINGS.add(CUSTOM_DEVICE_GESTURE_FEEDBACK_ENABLED);
             PRIVATE_SETTINGS.add(CUSTOM_DEVICE_FEATURE_SETTINGS);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_STATE);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_TYPE);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_AUTOHIDE_THRESHOLD);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_HIDEARROW);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_FONT_SIZE);
+            PRIVATE_SETTINGS.add(NETWORK_TRAFFIC_VIEW_LOCATION);
 	    PRIVATE_SETTINGS.add(QS_LAYOUT_ROWS_LANDSCAPE);
             PRIVATE_SETTINGS.add(STATUS_BAR_CLOCK);
             PRIVATE_SETTINGS.add(STATUSBAR_CLOCK_STYLE);
