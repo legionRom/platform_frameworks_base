f9cf31f65b8479e3913bbf9a93020bc8a4a039a6
Weijie Wang <weijiew@codeaurora.org>
2019-07-22 19:36:13 +0800

    SystemUI: Update the media notification only when needed
    The media notification is updated every second even if the
    screen is turned off, which causes unnecessary power consumption.
    So update the media notification only when needed
    
    Change-Id: I4d5bcc56fe804754eebec92d83397dac354ec012
    CRs-Fixed: 2493218
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- packages/SystemUI/src/com/android/systemui/statusbar/notification/row/wrapper/NotificationMediaTemplateViewWrapper.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/notification/row/wrapper/NotificationMediaTemplateViewWrapper.java
@@ -32,6 +32,7 @@ import android.text.format.DateUtils;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewStub;
+import android.view.ViewTreeObserver;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
