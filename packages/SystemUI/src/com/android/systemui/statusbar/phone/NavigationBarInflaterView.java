304d945c3af9588ac340e87e87d28f088d412af1
Andy CrossGate Yan <GeForce8800Ultra@gmail.com>
2019-11-03 03:32:02 +0000

    SystemUI: Revive navbar layout tuning via sysui_nav_bar tunable
    Google keeps fixing what ain't broken.
    This partially undoes commit e0d5ccd.
    
    Change-Id: I7a0fcfc9c27a08100712200d50f295f4bd81494d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/NavigationBarInflaterView.java
@@ -157,8 +157,7 @@ public class NavigationBarInflaterView extends FrameLayout
     @Override
     protected void onAttachedToWindow() {
         super.onAttachedToWindow();
-        Dependency.get(TunerService.class).addTunable(this, NAV_BAR_INVERSE);
-        Dependency.get(TunerService.class).addTunable(this, NAV_BAR_VIEWS);
+        Dependency.get(TunerService.class).addTunable(this, NAV_BAR_VIEWS, NAV_BAR_INVERSE);
     }
 
     @Override
@@ -170,7 +169,9 @@ public class NavigationBarInflaterView extends FrameLayout
 
     @Override
     public void onTuningChanged(String key, String newValue) {
-        if (NAV_BAR_INVERSE.equals(key)) {
+        if (NAV_BAR_VIEWS.equals(key)) {
+            setNavigationBarLayout(newValue);
+        } else if (NAV_BAR_INVERSE.equals(key)) {
             mInverseLayout = TunerService.parseIntegerSwitch(newValue, false);
             updateLayoutInversion();
         }
