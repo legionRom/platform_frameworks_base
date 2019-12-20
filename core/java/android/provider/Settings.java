1a4e0300a04c70971325cd819c10be95a8d9773a
Ankit-Gourav <Ankitgourav035@gmail.com>
2019-12-20 22:10:30 +0530

    TypoClock: User selected Font Style & Font Size [1/2]
    SDB-: Some Cleanups, separate configs for Text Clock Font Style & Size
    
    Co-authored-by: Ankit-Gourav <Ankitgourav035@gmail.com>
    Co-authored-by: SuperDroidBond <superdroidbond@yahoo.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5242,6 +5242,20 @@ public final class Settings {
         public static final String POWERMENU_TORCH = "powermenu_torch";
 
         /**
+         ** Change fonts for the system lockscreen clock widget
+         **
+         ** @hide
+         **/
+        public static final String CUSTOM_TEXT_CLOCK_FONTS = "custom_text_clock_fonts";
+
+        /**
+         * Change font size for the lockscreen text clock widget
+         *
+         * @hide
+         */
+        public static final String CUSTOM_TEXT_CLOCK_FONT_SIZE = "custom_text_clock_font_size";
+
+        /**
          * Disable dashboard conditions in settings
          * @hide
          */
