a7fbdb2be35bdbd8271a8922a51a4752261d7f91
00day0 <therandomuser11@gmail.com>
2020-01-01 21:19:16 +0100

    OneUI-like settings actionbar [1/3] todo: add settings observer so you won't need to restart settings app to toggle it
    Signed-off-by: 00day0 <therandomuser11@gmail.com>

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5124,6 +5124,13 @@ public final class Settings {
          */
         public static final String QS_BLUR = "qs_blur";
 
+
+	/**
+         * Some creation simillar to samsung OneUI
+         * @hide
+         */
+        public static final String SETTINGS_SPACER = "settings_spacer";
+
         /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
