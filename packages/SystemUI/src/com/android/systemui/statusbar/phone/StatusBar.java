e6398545309eca36277a2faf038ad0fe4ca3f4f9
SuperDroidBond <superdroidbond@yahoo.com>
2020-01-18 22:57:16 +0530

    TextClock: Transform Q Clock to X Clock [1/2]
    SDB: add a switch to align text clock to center
         add padding through java and remove from layout xml
         add layoutParams to support the new codes
    
    Special Thanks to @DennySPB for padding helps

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -4248,14 +4248,18 @@ public class StatusBar extends SystemUI implements DemoMode,
             resolver.registerContentObserver(Settings.System.getUriFor(
                     Settings.System.QS_HEADER_STYLE),
                     false, this, UserHandle.USER_ALL);
-        }
+            resolver.registerContentObserver(Settings.Secure.getUriFor(
+                    Settings.Secure.CENTER_TEXT_CLOCK),
+                    false, this, UserHandle.USER_ALL);
+	}
 
         @Override
         public void onChange(boolean selfChange, Uri uri) {
             if (uri.equals(Settings.System.getUriFor(Settings.System.FORCE_SHOW_NAVBAR))) {
                 updateNavigationBar(getRegisterStatusBarResult(), false);
 	    } else if (uri.equals(Settings.Secure.getUriFor(Settings.Secure.LOCKSCREEN_CLOCK_SELECTION)) ||
-		    uri.equals(Settings.Secure.getUriFor(Settings.Secure.LOCKSCREEN_DATE_SELECTION))) {
+		    uri.equals(Settings.Secure.getUriFor(Settings.Secure.LOCKSCREEN_DATE_SELECTION)) ||
+		    uri.equals(Settings.Secure.getUriFor(Settings.Secure.CENTER_TEXT_CLOCK))) {
                 updateKeyguardStatusSettings();
             } else if (uri.equals(Settings.System.getUriFor(Settings.System.QS_BLUR_ALPHA)) ||
                     uri.equals(Settings.System.getUriFor(Settings.System.QS_BLUR_INTENSITY))) {
