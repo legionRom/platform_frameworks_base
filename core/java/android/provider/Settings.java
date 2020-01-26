7ef5c0a5b5892a2664caa191877d68866f776898
Nauval Rizky <enuma.alrizky@gmail.com>
2020-01-26 17:31:14 +0900

    SystemUI: Introduce Adjustable Blur Intensity [1/2]

--- core/java/android/provider/Settings.java
+++ core/java/android/provider/Settings.java
@@ -5227,6 +5227,14 @@ public final class Settings {
         private static final Validator QS_BLUR_ALPHA_VALIDATOR = ANY_INTEGER_VALIDATOR;
 
         /**
+         * QS blur alpha
+         * @hide
+         */
+        public static final String QS_BLUR_INTENSITY = "qs_blur_intensity";
+        /** @hide */
+        private static final Validator QS_BLUR_INTENSITY_VALIDATOR = ANY_INTEGER_VALIDATOR;
+
+        /**
          * Settings to backup. This is here so that it's in the same place as the settings
          * keys and easy to update.
          *
@@ -5317,6 +5325,7 @@ public final class Settings {
             GESTURE_PILL_TOGGLE,
             QS_BLUR,
             QS_BLUR_ALPHA,
+            QS_BLUR_INTENSITY,
         };
 
         /**
@@ -5592,6 +5601,7 @@ public final class Settings {
             VALIDATORS.put(GESTURE_PILL_TOGGLE, GESTURE_PILL_TOGGLE_VALIDATOR);
             VALIDATORS.put(QS_BLUR, QS_BLUR_VALIDATOR);
             VALIDATORS.put(QS_BLUR_ALPHA, QS_BLUR_ALPHA_VALIDATOR);
+            VALIDATORS.put(QS_BLUR_INTENSITY, QS_BLUR_INTENSITY_VALIDATOR);
         }
 
         /**
