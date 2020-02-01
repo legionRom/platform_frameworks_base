9dc9cb4cac7940f561573907d596c164f9d9dfda
ezio84 <brabus84@gmail.com>
2020-02-01 13:10:57 -0500

    Port Lockscreen weather code to work with external/google [1/2]
    This is NOT the complete version done by jhenrique09 or ezio84,
    but rather an incremental patch. This was done in order to preserve
    original commit history of this feature.
    
    Change-Id: I915a94afea79e3c6c661bae5eda0a8c6e44d5a15

--- packages/SystemUI/src/com/android/systemui/SystemUIFactory.java
+++ packages/SystemUI/src/com/android/systemui/SystemUIFactory.java
@@ -50,7 +50,8 @@ import com.android.systemui.statusbar.phone.UnlockMethodCache;
 import com.android.systemui.statusbar.policy.KeyguardMonitor;
 import com.android.systemui.volume.VolumeDialogComponent;
 
-import java.util.concurrent.Executor;
+import com.google.android.systemui.LiveWallpaperScrimController;
+
 import java.util.function.Consumer;
 
 import dagger.Module;
@@ -140,8 +141,8 @@ public class SystemUIFactory {
             TriConsumer<ScrimState, Float, GradientColors> scrimStateListener,
             Consumer<Integer> scrimVisibleListener, DozeParameters dozeParameters,
             AlarmManager alarmManager, KeyguardMonitor keyguardMonitor) {
-        return new ScrimController(scrimBehind, scrimInFront, scrimStateListener,
-                scrimVisibleListener, dozeParameters, alarmManager, keyguardMonitor);
+        return new LiveWallpaperScrimController(scrimBehind, scrimInFront, lockscreenWallpaper,
+                scrimStateListener, scrimVisibleListener, dozeParameters, alarmManager, keyguardMonitor);
     }
 
     public NotificationIconAreaController createNotificationIconAreaController(Context context,
