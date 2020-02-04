76fca3e30579b0f4a332c88a30f437d6efac38f7
ezio84 <brabus84@gmail.com>
2020-02-04 23:16:57 +0100

    Lockscreen weather Soli port: more code improvements
    actually we have StatusBarGoogle being built from external google
    and it extends aosp StatusBar, so let's add the weather code in
    the aosp class instead of creating two StatusBarGoogle classes
    making the whole thing confusing
    
    Change-Id: Id77af04b5e17b9cbfd5db6a15a96177492f3ac9d

--- packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
+++ packages/SystemUI/src/com/android/systemui/statusbar/phone/StatusBar.java
@@ -254,6 +254,8 @@ import com.android.systemui.util.InjectionInflationController;
 import com.android.systemui.volume.VolumeComponent;
 
 import com.google.android.systemui.keyguard.KeyguardSliceProviderGoogle;
+import com.google.android.systemui.NotificationLockscreenUserManagerGoogle;
+import com.google.android.systemui.smartspace.SmartSpaceController;
 
 import java.io.FileDescriptor;
 import java.io.PrintWriter;
@@ -843,6 +845,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         int disabledFlags2 = result.mDisabledFlags2;
         Dependency.get(InitController.class).addPostInitTask(
                 () -> setUpDisableFlags(disabledFlags1, disabledFlags2));
+        ((NotificationLockscreenUserManagerGoogle) Dependency.get(NotificationLockscreenUserManager.class)).updateAodVisibilitySettings();
     }
 
     // ================================================================================
@@ -2724,10 +2727,7 @@ public class StatusBar extends SystemUI implements DemoMode,
         for (Map.Entry<String, ?> entry : Prefs.getAll(mContext).entrySet()) {
             pw.print("  "); pw.print(entry.getKey()); pw.print("="); pw.println(entry.getValue());
         }
-
-        if (mFlashlightController != null) {
-            mFlashlightController.dump(fd, pw, args);
-        }
+        SmartSpaceController.get(this.mContext).dump(fd, pw, args);
     }
 
     static void dumpBarTransitions(PrintWriter pw, String var, BarTransitions transitions) {
@@ -3007,9 +3007,8 @@ public class StatusBar extends SystemUI implements DemoMode,
             mLockscreenWallpaper.setCurrentUser(newUserId);
         }
         mScrimController.setCurrentUser(newUserId);
-        if (mWallpaperSupported) {
-            mWallpaperChangedReceiver.onReceive(mContext, null);
-        }
+        mWallpaperChangedReceiver.onReceive(mContext, null);
+        SmartSpaceController.get(this.mContext).reloadData();
     }
 
     /**
