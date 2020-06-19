a9e8a75c7ba1b2bbbb1297f8bc5fc96c793c015d
rajkale99 <kaleraj.rk@gmail.com>
2020-06-19 11:25:38 +0000

    Revert "Introduce Samsung like lockscreen clock"
    This reverts commit 6ddc6db6d81821211fbf13aaf8a0a4d2f8d59b6c.

--- packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
+++ packages/SystemUI/src/com/android/keyguard/clock/ClockManager.java
@@ -148,7 +148,6 @@ public final class ClockManager {
         LayoutInflater layoutInflater = injectionInflater.injectable(LayoutInflater.from(context));
 
         addBuiltinClock(() -> new DefaultClockController(res, layoutInflater, colorExtractor));
-        addBuiltinClock(() -> new SamsungClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SfunyClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new AnalogClockController(res, layoutInflater, colorExtractor));
         addBuiltinClock(() -> new SpectrumClockController(res, layoutInflater, colorExtractor));
